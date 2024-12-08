0c538c99cec4b036385e9925065f02a3a475e6e7
mpcooper23 <mpcooper23@gmail.com>
2024-12-07 19:11:30 -0600

    done

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -108,7 +108,7 @@ let board = '';
 for(let row = 0; row < size; row++){
   for(let col = 0; col < size; col++){
     if((row + col) % 2 === 0){
-      board += '';
+      board += ' ';
     }else {board += '#';
   }
 }
